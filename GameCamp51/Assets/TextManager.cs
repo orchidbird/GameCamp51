cf285c2191b4b41bfe8c3fa2a95f87d0e90087a9
mjmode1 <46835597+mjmode1@users.noreply.github.com>
2019-01-19 16:45:23 +0900

    엔터키를 누르면 자막이 변합니다
    -정민재-

---
+++ GameCamp51/Assets/TextManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public TextMesh text;
+    public int number = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            number++;
+            
+        }
+
+        if (number == 1)
+        {
+            text.text = "안녕!";
+        }
+        else if(number == 2)
+        {
+            text.text = "안녕!2";
+        }
+        else if (number == 3)
+        {
+            text.text = "안녕!3";
+        }
+    }
+}
