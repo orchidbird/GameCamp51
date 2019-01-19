e75d693f58a8b72901729ba89dab8283aa15e251
awist <46547066+awist@users.noreply.github.com>
2019-01-19 16:18:41 +0900

    button make

---
+++ GameCamp51/Assets/button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class button : MonoBehaviour
+{
+    public void StartBtn()
+    {
+        Debug.Log("StartBtn");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitBtn()
+    {
+        Application.Quit();
+    }
+   
+
+}
