aa61236064596123706b1c1b6c6d3af9eb4d1f8a
Nansae <orchidbird2@gmail.com>
2019-01-26 16:32:42 +0900

    세배 장면 + 확률적으로 돈 획득 기능

--- GameCamp51/Assets/GameManager.cs
+++ GameCamp51/Assets/GameManager.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour{
-    
+    public static int money;
 }
--- GameCamp51/Assets/TextManager.cs
+++ GameCamp51/Assets/TextManager.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class TextManager : MonoBehaviour{
     public TextMesh text;
+    public Text MoneyText;
     public int number = 0;
     string[] lines;
 
@@ -11,8 +14,26 @@ public class TextManager : MonoBehaviour{
     }
 
     void Update(){
-        if (Input.GetKeyDown(KeyCode.Return)) {number++;}
-        if (number < lines.Length)
+        MoneyText.text = "돈: " + GameManager.money;
+        if (Input.GetKeyDown(KeyCode.Return)){number++;}
+        
+        if (number >= lines.Length) return;
+        if (lines[number] == "Money"){
+            var money = Random.Range(0, 6);
+            if(money == 0)
+                GameManager.money += 10000;
+            else if(money == 1)
+                GameManager.money += 20000;
+            else if(money == 2)
+                GameManager.money += 30000;
+            else if(money == 3)
+                GameManager.money += 50000;
+            else if(money == 4)
+                GameManager.money += 300000;
+            else if(money == 5)
+                GameManager.money += 1000000;
+            number++;
+        }else
             text.text = lines[number];
     }
 }
