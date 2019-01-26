2bb3bae199e52da851dd86cddf9042c8ecb5eb47
Nansae <orchidbird2@gmail.com>
2019-01-26 16:17:53 +0900

    클릭 소리 기능 추가

---
+++ GameCamp51/Assets/SoundManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour{
+     public AudioSource audio;
+
+     void Update(){
+          if (Input.GetMouseButtonDown(0)){
+               audio.PlayOneShot(audio.clip);
+          }
+     }
+}
