cb2e87aafb9c876e6fff69a4327077d0439c1b79
Nansae <orchidbird2@gmail.com>
2019-02-09 15:16:39 +0900

    개발 중

--- GameCamp51/Assets/GameManager.cs
+++ GameCamp51/Assets/GameManager.cs
@@ -1,7 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour{
+    public TextManager TM;
+    public GameObject Choice;
     public static int money;
-    public static int sociability;
-    public static int intimacy;
+    public static bool friend;
+
+    public enum SelectionType{Club}
+
+    public void SetChoice(string yesText, string noText){
+        Choice.gameObject.SetActive(true);
+        var texts = Choice.GetComponentsInChildren<Text>();
+        texts[0].text = yesText;
+        texts[1].text = noText;
+    }
+    public void DoYes(){
+        TM.ClubText.gameObject.SetActive(true);
+        TM.ClubText.text = "동아리: 있음";
+        GoNext();
+    }
+    public void DoNo(){
+        TM.ClubText.gameObject.SetActive(true);
+        TM.ClubText.text = "동아리: 없음";
+        GoNext();
+    }
+
+    void GoNext(){
+        TM.number++;
+        Choice.gameObject.SetActive(false);
+    }
 }
